8aa8073203101aeb258834f203cc0ee054a92bb6
jdeuser <p8499@126.com>
2019-02-21 15:27:46 +0800

    node

--- src/main/java/p8499/style/drawable/.kt
+++ src/main/java/p8499/style/drawable/.kt
@@ -3,7 +3,7 @@ package p8499.style.drawable
 import p8499.style.DrawableSelector
 import p8499.style.Environment
 import p8499.style.Style
-import p8499.style.dp
+import p8499.style.color.Color
 import java.io.File
 import javax.xml.transform.sax.TransformerHandler
 
@@ -22,24 +22,31 @@ interface Node : Root
 fun Root.drawable() = ResourceDrawable(this)
 
 fun shape(vararg attrs: Pair<String, String>): Tag = Tag("shape", *attrs)
+fun rectangle(vararg attrs: Pair<String, String>): Tag = Tag("shape", android_shape("rectangle"), *attrs)
 fun solid(vararg attrs: Pair<String, String>): Tag = Tag("solid", *attrs)
+fun solid(color: Color): Tag = solid(android_color(color))
 fun padding(vararg attrs: Pair<String, String>): Tag = Tag("padding", *attrs)
+fun padding(top: String, bottom: String, left: String, right: String): Tag = padding(android_top(top), android_bottom(bottom), android_left(left), android_right(right))
+fun padding(padding: String) = padding(android_top(padding), android_bottom(padding), android_left(padding), android_right(padding))
 fun corners(vararg attrs: Pair<String, String>): Tag = Tag("corners", *attrs)
+fun corners(topLeftRadius: String, topRightRadius: String, bottomLeftRadius: String, bottomRightRadius: String): Tag = corners(android_topLeftRadius(topLeftRadius), android_topRightRadius(topRightRadius), android_bottomLeftRadius(bottomLeftRadius), android_bottomRightRadius(bottomRightRadius))
+fun corners(radius: String): Tag = corners(android_radius(radius))
 fun stroke(vararg attrs: Pair<String, String>): Tag = Tag("stroke", *attrs)
 fun item(vararg attrs: Pair<String, String>): Tag = Tag("item", *attrs)
 fun layer_list(vararg attrs: Pair<String, String>): Tag = Tag("layer-list", *attrs)
 
-
-fun shape(v: Any?) = "android:shape" to (v?.toString() ?: "@null")
-val shape_rectangle = shape("rectangle")
-fun top(v: Any?) = "android:top" to (v?.toString() ?: "@null")
-fun bottom(v: Any?) = "android:bottom" to (v?.toString() ?: "@null")
-fun left(v: Any?) = "android:left" to (v?.toString() ?: "@null")
-fun right(v: Any?) = "android:right" to (v?.toString() ?: "@null")
-fun color(v: Any?) = "android:color" to (v?.toString() ?: "@null")
-fun width(v: Any?) = "android:width" to (v?.toString() ?: "@null")
-fun topLeftRadius(v: Any?) = "android:topLeftRadius" to (v?.toString() ?: "@null")
-fun topRightRadius(v: Any?) = "android:topRightRadius" to (v?.toString() ?: "@null")
-fun bottomLeftRadius(v: Any?) = "android:bottomLeftRadius" to (v?.toString() ?: "@null")
-fun bottomRightRadius(v: Any?) = "android:bottomRightRadius" to (v?.toString() ?: "@null")
+fun android_shape(v: Any?) = "android:shape" to (v?.toString() ?: "@null")
+fun android_top(v: Any?) = "android:top" to (v?.toString() ?: "@null")
+fun android_bottom(v: Any?) = "android:bottom" to (v?.toString() ?: "@null")
+fun android_left(v: Any?) = "android:left" to (v?.toString() ?: "@null")
+fun android_right(v: Any?) = "android:right" to (v?.toString() ?: "@null")
+fun android_color(v: Any?) = "android:color" to (v?.toString() ?: "@null")
+fun android_width(v: Any?) = "android:width" to (v?.toString() ?: "@null")
+fun android_radius(v: Any?) = "android:radius" to (v?.toString() ?: "@null")
+fun android_topLeftRadius(v: Any?) = "android:topLeftRadius" to (v?.toString() ?: "@null")
+fun android_topRightRadius(v: Any?) = "android:topRightRadius" to (v?.toString() ?: "@null")
+fun android_bottomLeftRadius(v: Any?) = "android:bottomLeftRadius" to (v?.toString() ?: "@null")
+fun android_bottomRightRadius(v: Any?) = "android:bottomRightRadius" to (v?.toString() ?: "@null")
+fun android_dashGap(v: Any?) = "android:dashGap" to (v?.toString() ?: "@null")
+fun android_dashWidth(v: Any?) = "android:dashWidth" to (v?.toString() ?: "@null")
 
